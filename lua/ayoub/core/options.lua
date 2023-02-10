b67a86e9ab82d5bc4b7c6dbddcc5ed2459a15894
ayoubissaad <ayoub.issaad@gmail.com>
2023-02-10 17:15:53 +0100

    first setup

---
+++ lua/ayoub/core/options.lua
@@ -0,0 +1,47 @@
+local opt = vim.opt -- for conciseness
+
+-- line numbers
+opt.relativenumber = true
+opt.number = true
+
+-- tabs & indentation
+opt.tabstop = 2
+opt.shiftwidth = 2
+opt.expandtab = true
+opt.autoindent = true
+
+-- line wrapping
+opt.wrap = false
+
+-- search settings
+opt.ignorecase = true
+opt.smartcase = true
+
+-- cursor line
+opt.cursorline = true
+
+-- apearance
+opt.termguicolors = true
+opt.background = "dark"
+opt.signcolumn = "yes"
+
+-- backspace
+opt.backspace = "indent,eol,start"
+
+-- clipboard
+-- opt.clipboard:append("unnamedplus")
+
+-- split windows
+opt.splitright = true
+opt.splitbelow = true
+
+opt.iskeyword:append("-") -- adds dash (-) to the world, so when deleting a word containing a dash it deletes the whole word
+
+-- Set scrolloff to 8
+opt.scrolloff = 3
+
+-- Set an undo file to keep history of changes
+-- set undodir=~/.vim/undodir
+opt.undodir=vim.fn.stdpath('config') .. '/undodir'
+-- opt.undodir = "~/.config/nvim/undodir"
+opt.undofile = true
