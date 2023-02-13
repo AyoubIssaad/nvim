e5f6edbdeade9d345445b659553ad2bdcbd2476b
ayoubissaad <ayoub.issaad@gmail.com>
2023-02-13 21:15:45 +0100

    sumneko-->lua_ls

--- lua/ayoub/plugins/lsp/lspconfig.lua
+++ lua/ayoub/plugins/lsp/lspconfig.lua
@@ -91,7 +91,7 @@ require('lspconfig')['pyright'].setup{
 }
 
 -- configure lua server (with special settings)
-lspconfig["sumneko_lua"].setup({
+lspconfig["lua_ls"].setup({
   capabilities = capabilities,
   on_attach = on_attach,
   settings = { -- custom settings for lua
